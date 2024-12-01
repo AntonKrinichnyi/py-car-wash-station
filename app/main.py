2d4a8c5c280fea3847a0d0339375806313b4c202
AntonKrinichnyi <a.krinichnyi@gmail.com>
2024-12-01 12:16:53 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,34 @@
 class Car:
-    # write your code here
-    pass
+    def __init__(self, comfort_class, clean_mark, brand) -> None:
+        self.comfort_class = comfort_class
+        self.clean_mark = clean_mark
+        self.brand = brand
 
 
 class CarWashStation:
-    # write your code here
-    pass
+    def __init__(self, distance_from_city_center, clean_power, average_rating, count_of_ratings) -> None:
+        self.distance_from_city_center = distance_from_city_center
+        self.clean_power = clean_power
+        self.average_rating = average_rating
+        self.count_of_ratings = count_of_ratings
+
+    def serve_cars(self: list, cars: list) -> list:
+        return [car for car in cars if car.clean_mark < self.clean_power]
+    
+
+bmw = Car(comfort_class=3, clean_mark=3, brand='BMW')
+audi = Car(comfort_class=4, clean_mark=9, brand='Audi')
+
+print(bmw.clean_mark)  # 3
+
+wash_station = CarWashStation(
+    distance_from_city_center=5,
+    clean_power=6,
+    average_rating=3.5,
+    count_of_ratings=6
+)
+
+income = wash_station.serve_cars([bmw, audi])
+
+print(income)  # 6.3
+print(bmw.clean_mark)  # 6
